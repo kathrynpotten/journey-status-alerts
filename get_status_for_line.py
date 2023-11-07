3ea5aa336c56ef3b3b9a02b62513df9d2edec704
Kathryn Potten <kathrynpotten@users.noreply.github.com>
2023-11-07 17:38:50 +0000

    update single line status extraction to function

--- get_status_for_line.py
+++ get_status_for_line.py
@@ -3,21 +3,27 @@ import ast
 import sys
 
 
-# get line and stations for preferred route
-line = sys.argv[1]
+def get_status_for_line_section(line, departure, destination):
+    with open("stations.txt", "r", encoding="utf-8") as file:
+        line_stations = ast.literal_eval(file.read().split("\n")[0])[line]
 
-with open("stations.txt", "r", encoding="utf-8") as file:
-    line_stations = ast.literal_eval(file.read())[line]
+    for branch_stations in line_stations.values():
+        try:
+            idx1, idx2 = branch_stations.index(departure), branch_stations.index(
+                destination
+            )
+            stations = branch_stations[min(idx1, idx2) : max(idx1, idx2) + 1]
 
-for branch_stations in line_stations.values():
-    try:
-        idx1, idx2 = branch_stations.index(sys.argv[2]), branch_stations.index(
-            sys.argv[3]
-        )
-        stations = branch_stations[min(idx1, idx2) : max(idx1, idx2) + 1]
+        except ValueError:
+            continue
 
-    except ValueError:
-        continue
+    return f"{get_delay_status.get_status(line, stations)}"
 
-route_delay_status = f"{get_delay_status.get_status(line, stations)}"
-print(route_delay_status)
+
+if __name__ == "__main__":
+    # get line and stations for preferred route
+    line = sys.argv[1]
+    departure = sys.argv[2]
+    destination = sys.argv[3]
+
+    print(get_status_for_line_section(line, departure, destination))
