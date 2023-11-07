1d5fc2093b08eb70c4c354e563ef5d4bb656194c
Kathryn Potten <kathrynpotten@users.noreply.github.com>
2023-11-07 16:49:15 +0000

    update single route status extraction

--- get_status_for_line.py
+++ get_status_for_line.py
@@ -9,8 +9,15 @@ line = sys.argv[1]
 with open("stations.txt", "r", encoding="utf-8") as file:
     line_stations = ast.literal_eval(file.read())[line]
 
-idx1, idx2 = line_stations.index(sys.argv[2]), line_stations.index(sys.argv[3])
-stations = line_stations[min(idx1, idx2) : max(idx1, idx2) + 1]
-route_delay_status = f"{get_delay_status.get_status(line, stations)}"
+for branch_stations in line_stations.values():
+    try:
+        idx1, idx2 = branch_stations.index(sys.argv[2]), branch_stations.index(
+            sys.argv[3]
+        )
+        stations = branch_stations[min(idx1, idx2) : max(idx1, idx2) + 1]
 
+    except ValueError:
+        continue
+
+route_delay_status = f"{get_delay_status.get_status(line, stations)}"
 print(route_delay_status)
